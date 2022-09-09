1000bb2f02b82940f7b580f0735e93e774496964
fierroxido <101884944+fierroxido@users.noreply.github.com>
2022-09-08 19:23:08 -0500

    a
    a

--- recuperacion3/src/main/java/Herencias2/HerenciaII.java
+++ recuperacion3/src/main/java/Herencias2/HerenciaII.java
@@ -4,10 +4,41 @@
  */
 package Herencias2;
 
+import Herencias.movie;
+import Herencias.series;
+
 /**
  *
  * @author fierr
  */
 public class HerenciaII {
+    public static void main(String[] args) {
+        
+        movie Minions = new movie();
+        Minions.setTitulo("Minions");
+        Minions.setGenero("Dibujos animados");
+        Minions.setPremioOscar(false);
+        
+        
+        
+        series StivenFuture = new series();
+        StivenFuture.setTitulo("Stiven future");
+        StivenFuture.setGenero("Animada");
+        StivenFuture.setDuracionEpisodos(50);
+        StivenFuture.setTotalEpisodios(20);
+        StivenFuture.setTotalTemporadas(2);
+        
+        
+        System.out.println(Minions.toString());
+        System.out.println(StivenFuture.toString());
+        
+       Minions.play();
+       Minions.pausa();
+        
+        StivenFuture.play();
+        
+        
+        
+    }
     
 }
